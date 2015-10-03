7006b05e9025a4730dec41fc2a875ad15ae2f391
Sherman Cheung <shermancheung314@gmail.com>
2015-10-03 05:25:25 -0700

    Add current location button and correctly get lat/long

--- app/src/main/java/com/carcache/carcache/MapsActivity.java
+++ app/src/main/java/com/carcache/carcache/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.carcache.carcache;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +19,22 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity
+        implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        GoogleMap.OnMyLocationButtonClickListener,
+        OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    public static final String MAP_LOGGER = "MAP_LOGGER";
+    private GoogleApiClient mGoogleApiClient;
+    //private GoogleMap mMap;
+
+    private static final LocationRequest REQUEST = LocationRequest.create()
+            .setInterval(5000)          // 5 seconds
+            .setFastestInterval(16)     // 16ms = 60fps
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +44,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setOnMyLocationButtonClickListener(this);
+
+        /*
+        mMap = googleMap;
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+    }
+
+    /**
+     * Button to get current Location. This demonstrates how to get the current Location as required
+     * without needing to register a LocationListener
+     */
+    public void showMyLocation(View view) {
+        Log.v(MAP_LOGGER, "is connected: " + mGoogleApiClient.isConnected());
+        if (mGoogleApiClient.isConnected()) {
+            Log.v(MAP_LOGGER, "here");
+            Location loc = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            double lat = loc.getLatitude();
+            double lon = loc.getLongitude();
+            String msg = "Longitude: " + lon + " Latitude: " + lat;
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+            Log.v(MAP_LOGGER, msg);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        return false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        LocationServices.FusedLocationApi.requestLocationUpdates (
+                mGoogleApiClient,
+                REQUEST,
+                this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // Do nothing
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Do nothing
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
     }
 }
